8e6e8d45e76a6df8e2e28fc61f3688fbc2834e97
Melody-Lin <yiihsuanlin@gmail.com>
2026-01-28 19:47:46 -0800

    Exercise 3: Adding metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import BlogHero from '@/components/BlogHero';
+
 import { loadBlogPost } from '@/helpers/file-helpers';
+import { BLOG_TITLE } from '@/constants';
 
 import styles from './postSlug.module.css';
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params;
+  const { frontmatter } = await loadBlogPost(postSlug);
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
