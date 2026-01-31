7d1ad98daaa50019c6d7ceb697959b64f1335fd2
Melody-Lin <yiihsuanlin@gmail.com>
2026-01-30 23:33:12 -0800

    Exercise 7: Dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
+import { cookies } from 'next/headers';
 
-import { BLOG_TITLE, LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
+import {
+  BLOG_TITLE,
+  COLOR_THEME_COOKIE,
+  LIGHT_TOKENS,
+  DARK_TOKENS,
+} from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -25,9 +31,11 @@ const monoFont = Spline_Sans_Mono({
 
 export const metadata = { title: BLOG_TITLE };
 
-function RootLayout({ children }) {
-  // TODO: Dynamic theme depending on user preference
-  const theme = 'light';
+async function RootLayout({ children }) {
+  const savedTheme = (await cookies()).get(COLOR_THEME_COOKIE);
+  const theme = savedTheme?.value || 'light';
+
+  const themeColors = theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS;
 
   return (
     <RespectMotionPreferences>
@@ -35,10 +43,10 @@ function RootLayout({ children }) {
         lang='en'
         className={clsx(mainFont.variable, monoFont.variable)}
         data-color-theme={theme}
-        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+        style={themeColors}
       >
         <body>
-          <Header theme={theme} />
+          <Header initialTheme={theme} />
           <main>{children}</main>
           <Footer />
         </body>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,40 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
 import { Rss, Sun, Moon } from 'react-feather';
+import Cookie from 'js-cookie';
+
+import {
+  COLOR_THEME_COOKIE,
+  LIGHT_TOKENS,
+  DARK_TOKENS,
+} from '@/constants';
 
 import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './Header.module.css';
 
-function Header({ theme, className, ...delegated }) {
+function Header({ initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  function handleToggleTheme() {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+
+    Cookie.set(COLOR_THEME_COOKIE, nextTheme, {
+      expires: 1000,
+    });
+
+    const root = document.documentElement;
+    const colors = nextTheme === 'light' ? LIGHT_TOKENS : DARK_TOKENS;
+
+    root.setAttribute('data-color-theme', nextTheme);
+
+    Object.entries(colors).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+    });
+  }
   return (
     <header
       className={clsx(styles.wrapper, className)}
@@ -18,21 +45,21 @@ function Header({ theme, className, ...delegated }) {
       <div className={styles.actions}>
         <button className={styles.action}>
           <Rss
-            size="1.5rem"
+            size='1.5rem'
             style={{
               // Optical alignment
               transform: 'translate(2px, -2px)',
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+        <button className={styles.action} onClick={handleToggleTheme}>
+          {theme === 'light' ? (
+            <Sun size='1.5rem' />
+          ) : (
+            <Moon size='1.5rem' />
+          )}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
